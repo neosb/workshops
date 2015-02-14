e6d22673a7cda677a94cfd14f7e46d3e73f85d70
Simon Blaszczynski <simone@neosb.net>
2015-02-14 21:35:59 +0100

    Review model validations.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  # Validations
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :user_id, presence: true
 end
