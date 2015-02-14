158259b92ab52714d7d4c7987ff73f2c378e2746
Simon Blaszczynski <simone@neosb.net>
2015-02-14 21:30:52 +0100

    Validations and average rating for product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,20 @@
 class Product < ActiveRecord::Base
+  # Validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { :with => /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  # Calculate average rating
+  def average_rating
+    count = self.reviews.size
+    total = 0
+    self.reviews.each do |review|
+      total += review.rating
+    end
+    total / count.to_f
+  end
 end
