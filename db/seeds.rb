6cde65dd83b3aacdc5cf0676c47ac8b0b8fca976
Simon Blaszczynski <simone@neosb.net>
2015-02-15 10:27:27 +0100

    Fix typos with db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,20 @@ end
 o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
 ['mail@mail.com', 'myemail@mydomain.com', 'user@example.com', 'server@bot.com', 'girl@hot.com'].each do |email|
   string = (0...8).map { o[rand(o.length)] }.join
-  User.create!(email: email, password: '123', password_confirmation: '123', firstname: string, lastname: string.reverse)
+  if email == 'girl@hot.com'
+    User.create!(email: email, password: 'try_me', password_confirmation: 'try_me', firstname: string, lastname: string.reverse, admin: true)
+  else
+    User.create!(email: email, password: '123', password_confirmation: '123', firstname: string, lastname: string.reverse)
+  end
 end
-User.find_by(email: 'girl@hot.com').update(admin: true, password: 'try_me', password_confirmation: 'try_me')
 
 20.times do
   string = (0...8).map { o[rand(o.length)] }.join
   longer_string = (0...400).map { o[rand(o.length)] }.join
-  Product.create!(title: string, description: longer_string.gsub('e', ' '), price: rand(100.0).round(2), category_id: rand(1..6), user_id: 6)
+  Product.create!(title: string, description: longer_string.gsub('e', ' '), price: rand(100.0).round(2), category_id: rand(1..6), user_id: 5)
 end
 
 10.times do
   string = (0...200).map { o[rand(o.length)] }.join
-  Review.create!(content: string.gsub('e', ' '), rating: rand(1..100), product_id: rand(1..20), user_id: 6)
+  Review.create!(content: string.gsub('e', ' '), rating: rand(1..100), product_id: rand(1..20), user_id: rand(1..4))
 end
