5d992822285f02c19b2dcd1b640fb69a47eb3d8f
Simon Blaszczynski <simone@neosb.net>
2015-02-15 08:34:10 +0100

    Initial seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['creative category', 'lulz', 'rails girls', 'linux vs mac', 'cloud', 'blood-thirsty hummingbirds'].each do |category|
+  Category.create!(name: category)
+end
+
+o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+['mail@mail.com', 'myemail@mydomain.com', 'user@example.com', 'server@bot.com', 'girl@hot.com'].each do |email|
+  string = (0...8).map { o[rand(o.length)] }.join
+  User.create!(email: email, password: '123', password_confirmation: '123', firstname: string, lastname: string.reverse)
+end
+User.find_by(email: 'girl@hot.com').update(admin: true, password: 'try_me', password_confirmation: 'try_me')
+
+20.times do
+  string = (0...8).map { o[rand(o.length)] }.join
+  longer_string = (0...400).map { o[rand(o.length)] }.join
+  Product.create!(title: string, description: longer_string.gsub('e', ' '), price: rand(100.0).round(2), category_id: rand(1..6), user_id: rand(1..5))
+end
+
+10.times do
+  string = (0...200).map { o[rand(o.length)] }.join
+  Review.create!(content: string.gsub('e', ' '), rating: rand(1..100), product_id: rand(1..20), user_id: rand(1..5))
+end
